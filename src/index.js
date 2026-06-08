673ed6be9893bf3bdfce1bbf6124701c84658caf
Hajra <hajramazhar003@icloud.com>
2026-06-08 04:29:40 -0700

    Add new allowed origin for production URLs

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ app.use(cors({
       'http://localhost:3000',
       'http://localhost:5000',
       'https://project-veracity-backend-initial-production-be6f.up.railway.app',
+      'https://project-veracity-backend-initial-production-71b7.up.railway.app',
     ];
  
     // Allow any Vercel URL — covers all preview + production URLs
